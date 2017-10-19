4600dae0d382669348c3684b5373708e471662fe
Flavio <flavio_henrique457@hotmail.com>
2017-10-19 17:23:23 -0200

    JavaDoc das classes Usuário e Movimentação

--- Etapa2/src/main/java/com/mycompany/etapa2/modelo/Movimentacao.java
+++ Etapa2/src/main/java/com/mycompany/etapa2/modelo/Movimentacao.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+/**
+ * Classe modelo para movimentações financeiras
+ *
+ * @author Flavio
+ */
 public class Movimentacao implements Serializable {
 
     private String descricao;
@@ -13,6 +18,16 @@ public class Movimentacao implements Serializable {
     private LocalDate data;
     private String email;
 
+    /**
+     * Construtor de Movimentacao
+     *
+     * @param descricao é a descrição daquela movimentação
+     * @param categoria é a categoria daquela movimentação
+     * @param valor é o valor daquela movimentação
+     * @param tipo é o tipo da movimentação
+     * @param data é a data em que a movimentação ocorreu ou ocorrerá
+     * @param email é o email do usuário que registrou a movimentação
+     */
     public Movimentacao(String descricao, String categoria, float valor,
             String tipo, LocalDate data, String email) {
         this.descricao = descricao;
@@ -27,46 +42,99 @@ public class Movimentacao implements Serializable {
     public Movimentacao() {
     }
 
+    /**
+     * Busca a descrição da movimentação
+     *
+     * @return retorna a descrição da movimentação
+     */
     public String getDescricao() {
         return descricao;
     }
 
+    /**
+     * Modifica ou salva uma descrição para uma movimentação
+     *
+     * @param descricao é a descrição passada para a movimentação
+     */
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
+    /**
+     * Busca a categoria da movimentação
+     *
+     * @return a categoria da movimentação
+     */
     public String getCategoria() {
         return categoria;
     }
 
+    /**
+     * Altera ou salva uma categoria para uma movimentação
+     *
+     * @param categoria é a categoria passada para a movimentação
+     */
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
 
+    /**
+     * Retorna o valor da movimentação
+     *
+     * @return retorna o valor
+     */
     public float getValor() {
         return valor;
     }
 
+    /**
+     * Altera ou salva um valor para uma movimentação
+     *
+     * @param valor é o valor para aquela movimentação
+     */
     public void setValor(float valor) {
         this.valor = valor;
     }
 
+    /**
+     * Busca o tipo da movimentação
+     *
+     * @return retorna o tipo da movimentação
+     */
     public String getTipo() {
         return tipo;
     }
 
+    /**
+     * Salva ou altera o tipo de uma movimentação
+     *
+     * @param tipo é o tipo passado para aquela movimentação
+     */
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
 
+    /**
+     * Busca a data em que uma movimentação aconteceu
+     *
+     * @return a data da movimentação
+     */
     public LocalDate getData() {
         return data;
     }
 
+    /**
+     * Altera ou salva a data de uma movimentação
+     * @param data é a data que foi passada para uma movimentação
+     */
     public void setData(LocalDate data) {
         this.data = data;
     }
 
+    /**
+     * Busca o email do usuário que realizou a movimentação
+     * @return o email do usuário
+     */
     public String getEmail() {
         return email;
     }
@@ -74,7 +142,12 @@ public class Movimentacao implements Serializable {
     public void setEmail(String email) {
         this.email = email;
     }
-  
+    
+    
+    /**
+     * Transforma a movimentação em uma string com seus atributos
+     * @return uma string com os atributos da movimentação
+     */
     @Override
     public String toString() {
         return "Movimentacao{" + "descricao=" + descricao + ", categoria=" + categoria + ", valor=" + valor + ", tipo=" + tipo + ", data=" + data + ", email=" + email + '}';
@@ -92,6 +165,11 @@ public class Movimentacao implements Serializable {
         return hash;
     }
 
+    /**
+     * Compara uma movimentação com outra e determina se são iguais
+     * @param obj é a movimentação que será comparada com a atual
+     * @return 
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
--- Etapa2/src/main/java/com/mycompany/etapa2/modelo/Usuario.java
+++ Etapa2/src/main/java/com/mycompany/etapa2/modelo/Usuario.java
@@ -9,6 +9,11 @@ import java.util.List;
 
 import java.util.Objects;
 
+/**
+ * Classe modelo para usuários da aplicação
+ * @author Flavio
+ */
+
 public class Usuario implements Serializable {
 
     private String nome;
@@ -18,6 +23,14 @@ public class Usuario implements Serializable {
     private String senha;
     private List<Movimentacao> movimentacoes;
 
+    /**
+     * Construtor de Usuario
+     * @param nome nome do usuário
+     * @param sexo sexo do usuário
+     * @param nascimento Data de nascimento do usuário
+     * @param email Email do usuário
+     * @param senha  Senha do usuário
+     */
     public Usuario(String nome, String sexo, LocalDate nascimento, String email, String senha) {
         this.nome = nome;
         this.sexo = sexo;
@@ -27,67 +40,135 @@ public class Usuario implements Serializable {
         movimentacoes = new ArrayList<>();
     }
 
+    /**
+     * Constutor sem parâmetros de usuário, apenas instancia a lista de movimentações
+     */
     public Usuario() {
         movimentacoes = new ArrayList<>();
     }
-
+    
+    /**
+     * Busca o nome de um usuário
+     * @return retorna o nome do usuário
+     */
     public String getNome() {
         return nome;
     }
-
+    
+    /**
+     * Altera ou salva o nome do usuário
+     * @param nome é o nome passado
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
-
+    
+    /**
+     * Busca o sexo do usuário
+     * @return  retorna o sexo
+     */
     public String getSexo() {
         return sexo;
     }
-
+    
+    /**
+     * Altera ou salva o sexo do usuário
+     * @param sexo é o sexo do usuário
+     */
     public void setSexo(String sexo) {
         this.sexo = sexo;
     }
 
+    /**
+     * Busca a data de nascimento do usuário
+     * @return retorna a data de nascimento do usuário
+     */
     public LocalDate getNascimento() {
         return nascimento;
     }
-
+    
+    /**
+     * Altera ou salva a data de nascimento de um usuário
+     * @param nascimento data de nascimento passada
+     */
     public void setNascimento(LocalDate nascimento) {
         this.nascimento = nascimento;
     }
-
+    
+    /**
+     * Busca o email do usuário
+     * @return retorna o email
+     */
     public String getEmail() {
         return email;
     }
-
+    
+    /**
+     * Altera ou salva o email de um usuário
+     * @param email é o email passado
+     */
     public void setEmail(String email) {
         this.email = email;
     }
-
+    
+    /**
+     * Busca a senha do usuário
+     * @return retorna a senha
+     */
     public String getSenha() {
         return senha;
     }
 
+    /**
+     * Altera ou salva a senha de um usuário
+     * @param senha é a senha informada
+     */    
     public void setSenha(String senha) {
         this.senha = senha;
     }
-
+    
+    /**
+     * Busca as movimentações daquele usuário
+     * @return retorna uma lista de movimentações
+     */
     public List<Movimentacao> getMovimentacoes() {
         return movimentacoes;
     }
-
+    
+    /**
+     * Salva as movimentações daquele usuário
+     * @param movimentacoes é a lista de movimentações que serão salvas
+     */
     public void setMovimentacoes(List<Movimentacao> movimentacoes) {
         this.movimentacoes = movimentacoes;
     }
-
+    
+    /**
+     * Salva uma movimentação na lista de movimentações
+     * @param m é a movimentação que será salva
+     * @return retorna verdadeiro ou falso de acordo com o resultado do método 'add'
+     */
     public boolean salvarMov(Movimentacao m) {
 
         return movimentacoes.add(m);
     }
     
+    /**
+     * Deleta uma movimentação específica
+     * @param m é a movimentação que será deletada
+     * @return retorna o resultado do metodo 'remove', verdadeiro ou falso.
+     */
     public boolean deletarMov(Movimentacao m){
         return movimentacoes.remove(m);
     }
-
+    
+    /**
+     * Atualiza uma determinada movimentação
+     * @param atual é a movimentação que está salva na lista
+     * @param nova é a movimentação que será salva no lugar da atual
+     * @return retorna verdadeiro ou falso, caso seja salva ou não
+     * @throws CadastroException 
+     */
     public boolean atualizarMov(Movimentacao atual, Movimentacao nova) throws CadastroException {
 
         if (nova.getDescricao().equals("")) {
