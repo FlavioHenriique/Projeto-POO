7b603a803d9b24cc10c6bd3017c3cc22d411e642
Flavio <flavio_henrique457@hotmail.com>
2017-10-19 17:05:22 -0200

    JavaDoc das exceções

--- Etapa2/src/main/java/com/mycompany/etapa2/excecoes/CadastroException.java
+++ Etapa2/src/main/java/com/mycompany/etapa2/excecoes/CadastroException.java
@@ -1,16 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.etapa2.excecoes;
 
 /**
- *
+ * Exceção criada para erros no cadastro de movimentações e usuários 
  * @author Flavio
  */
 public class CadastroException extends Exception {
-
+    
+    /**
+     * Construtor de CadastroException
+     * @param mensagem  é a mensagem que será passada, de acordo com o erro ocorrido
+     */
     public CadastroException(String mensagem) {
         super(mensagem);
     }
--- Etapa2/src/main/java/com/mycompany/etapa2/excecoes/EmailException.java
+++ Etapa2/src/main/java/com/mycompany/etapa2/excecoes/EmailException.java
@@ -1,17 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.etapa2.excecoes;
 
 /**
- *
+ * Exceção criada para tentativas de cadastro de usuários com emails já existentes
  * @author Flavio
  */
 
 public class EmailException extends Exception{
     
+    /**
+     * Construtor de EmailException
+     * @param mensagem é a mensagem que será enviada caso a exceção ocorra.
+     */
     public EmailException(String mensagem){
         super(mensagem);
     }
